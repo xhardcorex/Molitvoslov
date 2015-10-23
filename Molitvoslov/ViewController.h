68f43318213405ee5a7f68182c5bed691ea11ec7
xhardcorex <nikshebeda@gmail.com>
2015-10-23 23:30:12 +0300

    fg

---
+++ Molitvoslov/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Molitvoslov
+//
+//  Created by Nik on 23.10.15.
+//  Copyright © 2015 Nik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Molitvoslov/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Molitvoslov
+//
+//  Created by Nik on 23.10.15.
+//  Copyright © 2015 Nik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
